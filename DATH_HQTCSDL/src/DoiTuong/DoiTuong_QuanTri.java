2cb198be7cf995bb287ce44b3acd1f638f2460b4
Nguyen Duc Huy <duchuy040421@gmail.com>
2021-11-18 10:54:58 +0700

    Them tinh nang tao, xoa, sua login khi tao tk

--- DATH_HQTCSDL/src/DoiTuong/DoiTuong_QuanTri.java
+++ DATH_HQTCSDL/src/DoiTuong/DoiTuong_QuanTri.java
@@ -3,6 +3,7 @@ package DoiTuong;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 import KetNoi.KetNoi;
 
@@ -37,6 +38,81 @@ public class DoiTuong_QuanTri {
 		this.tinhtrang = tinhtrang;
 	}
 	
+	public void themLogin(DoiTuong_QuanTri tk, String taikhoan, String matkhau) {
+		KetNoi kn = new KetNoi();
+		String sql_1 = "EXEC sp_addLogin '"+tk.getTaikhoan()+"','"+tk.getMatkhau()+"','QLCH'";
+		String sql_2 = "CREATE USER "+tk.getTaikhoan()+" FOR LOGIN "+tk.getTaikhoan();
+		String sql_3 = "ALTER ROLE "+tk.getPhanhe() + " ADD member "+tk.getTaikhoan();
+		Connection cnn = kn.getConnection();
+		
+		try {
+			Statement stmt = cnn.createStatement();
+			
+			stmt.executeUpdate(sql_1);
+			stmt.executeUpdate(sql_2);
+			stmt.executeUpdate(sql_3);
+			stmt.close();
+			cnn.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		kn.Disconnect();
+	}
+	
+	public void xoaLogin(DoiTuong_QuanTri tk, String taikhoan, String matkhau) {
+		KetNoi kn = new KetNoi();
+		String sql_0 = "USE [master]; "
+				+ "DECLARE @kill varchar(8000) = '';  "
+				+ "SELECT @kill = @kill + 'kill ' + CONVERT(varchar(5), session_id) + ';'  "
+				+ "FROM sys.dm_exec_sessions "
+				+ "WHERE database_id  = db_id('MyDB') "
+				+ "EXEC(@kill);";
+		String sql_1 = "DROP USER "+tk.getTaikhoan();
+		String sql_2 = "DROP LOGIN "+tk.getTaikhoan();
+		Connection cnn = kn.getConnection();
+		
+		try {
+			Statement stmt = cnn.createStatement();
+			
+			stmt.executeUpdate(sql_1);
+			stmt.executeUpdate(sql_0);
+			stmt.executeUpdate(sql_2);
+			stmt.close();
+			cnn.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		kn.Disconnect();
+	}
+	
+	public void suaLogin(DoiTuong_QuanTri tk, String taikhoan, String matkhau) {
+		KetNoi kn = new KetNoi();
+		String sql_1 = "DROP USER "+tk.getTaikhoan();
+		String sql_2 = "CREATE USER "+tk.getTaikhoan()+" FOR LOGIN "+tk.getTaikhoan();
+		String sql_3 = "ALTER ROLE "+tk.getPhanhe() + " ADD member "+tk.getTaikhoan();
+		String sql_4 = "ALTER LOGIN "+tk.getTaikhoan() + 
+				" WITH PASSWORD = '"+tk.getMatkhau()+"'";
+		
+		Connection cnn = kn.getConnection();
+		
+		try {
+			Statement stmt = cnn.createStatement();
+			
+			stmt.executeUpdate(sql_1);
+			stmt.executeUpdate(sql_2);
+			stmt.executeUpdate(sql_3);
+			stmt.executeUpdate(sql_4);
+			stmt.close();
+			cnn.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		kn.Disconnect();
+	}
+	
 	public boolean themTaiKhoan(DoiTuong_QuanTri tk, String taikhoan, String matkhau) {
 		KetNoi kn = new KetNoi(taikhoan, matkhau);
 		boolean result = false;
@@ -50,13 +126,16 @@ public class DoiTuong_QuanTri {
 			stmt.setString(2, tk.getMatkhau());
 			stmt.setString(3, tk.getPhanhe());
 			stmt.setString(4, tk.getTinhtrang());
-			
+		
 			result = stmt.executeUpdate()>0;
+			themLogin(tk,taikhoan,matkhau);
+			
 			stmt.close();
 			cnn.close();
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
+		
 		
 		kn.Disconnect();
 		
@@ -75,6 +154,8 @@ public class DoiTuong_QuanTri {
 			stmt.setString(1, tk.getTaikhoan());
 			
 			result = stmt.executeUpdate()>0;
+			xoaLogin(tk,taikhoan,matkhau);
+			
 			stmt.close();
 			cnn.close();
 		} catch (SQLException e) {
@@ -103,6 +184,8 @@ public class DoiTuong_QuanTri {
 			stmt.setString(5, tk.getTaikhoan());
 			
 			result = stmt.executeUpdate()>0;
+			suaLogin(tk,taikhoan,matkhau);
+			
 			stmt.close();
 			cnn.close();
 		} catch (SQLException e) {
